062a2511708823edfb2fddb47c8e9bab39fc08cd
Aniket Shukla <aniket14042001@gmail.com>
2023-05-12 07:01:56 +0000

    upddated

--- master_driver.sv
+++ master_driver.sv
@@ -10,8 +10,34 @@ class master_driver extends uvm_driver #(master_seq_i);
         super.build_phase(phase);
         // config data line
     endfunction
+
+    function void connect_phase(uvm_phase phase);
+
+    endfunction
+
     task run_phase(uvm_phase phase);
 
     endtask
+
+    function send_to_dut(master_seq_i seqi);
+        
+    endfunction
+    function write_addr(master_seq_i seqi);
+
+    endfunction
+
+    function write_data(master_seq_i seqi);
+
+    endfunction
+
+    function read_addr(master_seq_i seqi);
+
+    endfunction
+    function write_resp(master_seq_i seqi);
+
+    endfunction
+    function read_data(master_seq_i seqi);
+
+    endfunction
 
 endclass
