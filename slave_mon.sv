29bbc164ab9ff1a090b3ebd5612c97d6b722af70
Aniket Shukla <aniket14042001@gmail.com>
2023-05-24 07:23:22 +0000

    update slave monitor

--- slave_mon.sv
+++ slave_mon.sv
@@ -2,17 +2,45 @@ class slave_mon extends uvm_monitor #(slave_seq_i);
 
     `uvm_component_utils(slave_mon)
   
-
+	virtual axi_if.S_MON_MP vif;
+  
+  slave_seq_i store[int][int];
+  int ID[int];
   
     function new(string name="slave_mon",uvm_component parent=null);
     super.new(name,parent);
     endfunction
 
+  function void build_phase(uvm_phase phase);
+    
+    
+  endfunction 
   
-
-    // task run_phase(uvm_phase phase);
-
-
-    // endtask
+  
+  function void connect_phase(uvm_phase phase);
+    
+    
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    
+  endtask
+  
+  
+  task collect();
+    
+    
+  endtask
+  
+  task collect_data();
+    
+    
+  endtask
+  
+  task collect_resp();
+    
+  endtask
+  
+  
 
 endclass
